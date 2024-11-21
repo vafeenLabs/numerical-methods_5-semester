857f50b7111290b58d594bb4f224d89ee9f1894a
Vafeen <666av6@gmail.com>
2024-11-21 19:20:16 +0300

    21 11 2024 19:20:16

---
+++ lab2/Source.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+void printRectMatrix(int N, int L, double** A, double* f)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < L; j++)
+        {
+            cout << A[i][j] << '\t';
+        }
+        cout << f[i] << endl;
+    }
+    for (int i = 0; i < 40; i++)
+        cout << '-';
+    cout << endl;
+}
+
+void printRectMatrixRounded(int N, int L, double** A, double* f)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < L; j++)
+        {
+            cout << round(A[i][j] * 100.0) / 100.0 << '\t';
+        }
+        cout << f[i] << endl;
+    }
+    for (int i = 0; i < 40; i++)
+        cout << '-';
+    cout << endl;
+}
+
+// Функция для решения СЛАУ с нижнеленточной матрицей
+double* solveBottomBandMatrix(int N, int L, double** A, double* f)
+{
+    double mult;
+    double* cur_bottom = new double[L - 1];
+    // Прямой ход метода Гаусса с единственным делением
+    for (int i = 0; i < N; i++)
+    {
+        double diag = A[i][L - 1];
+        A[i][L - 1] = 1.0;
+        f[i] /= diag;
+
+        for (int k = 1; k < L && i + k < N; k++) // сохраняем множители для операций со строками
+        {
+            cur_bottom[k - 1] = A[i + k][L - k - 1];
+        }
+
+        for (int j = 1; j < L && i + j < N; j++)
+        {
+            A[i + j][L - j - 1] /= diag;
+        }
+
+        for (int j = 1; j < L && i + j < N; j++) // вычитание текущей строки из следующих
+        {
+            mult = cur_bottom[j - 1];
+            for (int k = j; k < L && i + k < N; k++)
+            {
+                A[i + k][L - (k - j) - 1] -= mult * A[i + k][L - k - 1];
+                //printRectMatrixRounded(N, L, A, f);
+            }
+            mult = cur_bottom[j - 1];
+            f[i + j] -= f[i] * mult;
+
+            printRectMatrixRounded(N, L, A, f);
+        }
+
+    }
+
+    // Обратный ход
+    double* x = new double[N];
+    for (int i = N - 1; i >= 0; i--)
+    {
+        x[i] = f[i];
+
+        for (int k = i + 1; k < N && k - i < L; k++)
+        {
+            x[i] -= A[k][L - (k - i) - 1] * x[k];
+        }
+        //x[i] /= A[i][L - 1];
+    }
+
+    delete[] cur_bottom;
+
+    return x;
+}
+
+
+double* solveBottomBandMatrixWithoutDivision(int N, int L, double** A, double* f)
+{
+    double mult;
+    double* cur_bottom = new double[L - 1];
+    // Прямой ход метода Гаусса с единственным делением
+    for (int i = 0; i < N; i++)
+    {
+        double diag = A[i][L - 1];
+        
+        
+        //cout << "cur_bottom: ";
+        for (int k = 1; k < L && i + k < N; k++) // сохраняем множители для операций со строками
+        {
+            cur_bottom[k - 1] = A[i + k][L - k - 1];
+            //cout << cur_bottom[k - 1] << ' ';
+        }
+        cout << endl;
+        
+
+        for (int j = 1; j < L && i + j < N; j++) // вычитание текущей строки из следующих
+        {
+
+            for (int k = j; k < L && i + k < N; k++)
+            {
+                mult = cur_bottom[k - 1] / diag;
+                //cout << cur_bottom[j - 1] << ' ' << diag << ' ' << "mult = " << mult << endl;
+                A[i + k][L - (k - j) - 1] -= mult * cur_bottom[j - 1];
+                //printRectMatrixRounded(N, L, A, f);
+            }
+            mult = cur_bottom[j - 1] / diag;
+            f[i + j] -= f[i] * mult;
+
+            printRectMatrixRounded(N, L, A, f);
+        }
+        
+    }
+
+    // Обратный ход
+    double* x = new double[N];
+    for (int i = N - 1; i >= 0; i--)
+    {
+        x[i] = f[i];
+
+        for (int k = i + 1; k < N && k - i < L; k++)
+        {
+            x[i] -= A[k][L - (k - i) - 1] * x[k];
+        }
+        x[i] /= A[i][L - 1];
+    }
+
+    delete[] cur_bottom;
+
+    return x;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    ifstream inputFile("test.txt");
+    if (!inputFile)
+    {
+        cerr << "Ошибка: не удалось открыть файл input.txt" << endl;
+        return 1;
+    }
+
+    // Считываем размерность и половину ширины ленты
+    int N, L;
+    inputFile >> N >> L;
+
+    // Создаем динамический массив для ленты матрицы A и для вектора правой части f
+    double* f = new double[N];
+    double** A = new double* [N];
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = new double[L];
+        for (int j = 0; j < L; j++)
+        {
+            inputFile >> A[i][j];
+        }
+        inputFile >> f[i];
+    }
+
+    // Закрываем файл
+    inputFile.close();
+
+    printRectMatrix(N, L, A, f);
+    // Решение системы
+    double* x = solveBottomBandMatrix(N, L, A, f);
+
+    // Вывод результата
+    cout << "Решение системы:" << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cout << "x[" << i << "] = " << x[i] << endl;
+    }
+
+    // Освобождение памяти
+    for (int i = 0; i < N; i++)
+    {
+        delete[] A[i];
+    }
+    delete[] A;
+    delete[] f;
+    delete[] x;
+    return 0;
+}
